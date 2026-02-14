ef766997a786d660fb9e6f20a60da1be80f76dfb
Gyorgy Barabas <dysordys@protonmail.com>
2026-02-14 15:24:44 +0100

    Minor update to examples/evo-demo-2D.jl

--- examples/evo-demo-2D.jl
+++ examples/evo-demo-2D.jl
@@ -15,10 +15,10 @@ config = EcoEvoConfig(
 
 lineage = Community([Species(1.0, [0.3, -0.3])], PopulationSize{Float64}[], 0.0)
 lineage = ecoDyn(lineage, config)
-@time lineage = evolve!(lineage, config, 10000);
+@time lineage = evolve!(lineage, config, 3000);
 
 plotEvo(lineage) # Shorthand for the one below; they should yield identical plots
 plotEvo(lineage, traitDim=1) # Should be indentical to the one above
-plotEvo(lineage, traitDim=2)
-plotEvoTwoTrait(lineage, camera=(60, 25))
-plotEvoTwoTraitInteractive(lineage)
+plotEvo(lineage, traitDim=2) # Plots the second trait instead of the first
+plotEvoTwoTrait(lineage, camera=(60, 25)) # Static 3D plot of the two traits
+plotEvoTwoTraitInteractive(lineage) # Interactive 3D plot of the two traits
